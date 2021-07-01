102733d6d6a483114505371c523dfc11be533997
Habibur Rahman <habib@inverseai.com>
2021-07-02 02:12:52 +0600

    Prepare Tab view with blank view items

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Office on 02/07/2021.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Video List")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
