8ac853a914365ccc0dcdc93be4fd4637d0ed77cc
Habibur Rahman <habib@inverseai.com>
2021-07-02 01:20:23 +0600

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Office on 02/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
