4615534c3f84229e574f1aa29a37c3154400464b
Habibur Rahman <habib@inverseai.com>
2021-07-22 01:22:42 +0600

    Implementation of location view

---
+++ Africa/Model/LocationModel.swift
@@ -0,0 +1,21 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Office on 22/07/2021.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
